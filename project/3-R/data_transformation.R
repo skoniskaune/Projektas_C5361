21d13d4c7cae0ee11f28a7eee388fdc79c6d6da8
skoniskaune <e.gridziuska@gmail.com>
2023-12-27 15:50:19 +0200

    Update data_transformation.R

--- project/3-R/data_transformation.R
+++ project/3-R/data_transformation.R
@@ -1,3 +1,14 @@
 library(readr)
-read_csv("../1-data/put_your_data_files_here.csv")
-### save combined file into 1-data directory
+library(tidyverse)
+
+data <- read_csv("../1-data/1-sample_data.csv")
+data_additional <- read_csv("../1-data/3-additional_features.csv")
+data_additional2 <- read_csv("../1-data/2-additional_data.csv")
+
+head(data)
+head(data_additional2)
+
+appended_data <- rbind(data,data_additional2)
+
+joined_data <- inner_join(appended_data, data_additional, by = "id")
+write_csv(joined_data, "../1-data/train_data.csv")
