a86782ba888a8d5a2cec9fb38c66f34693c56659
skoniskaune <151195634+skoniskaune@users.noreply.github.com>
2023-12-28 17:00:57 +0200

    Delete practice directory

--- practice/Kaunas/3-R/data_transformation.R
+++
@@ -1,8 +0,0 @@
-#install.packages("tidyverse")
-library(tidyverse)
-
-data <- read_csv("../../../project/1-data/1-sample_data.csv")
-data_additional <- read_csv("../../../project/1-data/3-additional_features.csv")
-
-joined_data <- inner_join(data, data_additional, by = "id")
-write_csv(joined_data, "../../../project/1-data/train_data.csv")
