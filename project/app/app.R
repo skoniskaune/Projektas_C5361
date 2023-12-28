458e28763bd400e57b38afd716a38a08e7ddf4f4
skoniskaune <151195634+skoniskaune@users.noreply.github.com>
2023-12-28 16:26:28 +0200

    Update app.R

--- project/app/app.R
+++ project/app/app.R
@@ -1,14 +1,34 @@
 library(shiny)
+library(tidyverse)
+library(h2o)
+h2o.init()
+
 ui <- fluidPage(
-  titlePanel("Hello world"),
+  titlePanel("Banking APP"),
   sidebarLayout(
     sidebarPanel(
+      fileInput("file", "Upload CSV file")
     ),
     mainPanel(
+      tableOutput("table")
     )
   )
 )
-server <- function(input, output) {
 
+server <- function(input, output) {
+  model <- h2o.loadModel("../../4-model/rf_model1")
+  output$table <- renderTable({
+    req(input$file)
+    test_data <- h2o.importFile(input$file$datapath)
+    predictions <- h2o.predict(model, test_data)
+    predictions %>%
+      as_tibble() %>%
+      mutate(id = row_number(), y = p0) %>%
+      select(id, y) %>%
+      head(10)
+  })
+  
+  
 }
 shinyApp(ui = ui, server = server)
+
